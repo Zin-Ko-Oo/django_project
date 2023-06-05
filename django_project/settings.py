1e4687a49382dfe64c0bff39aee3b5faf3790828
Zin-Ko-Oo <zko99309@gmail.com>
2023-06-05 10:29:29 +0630

    New Update for Heroku development

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-@i0@!_yf&bgwtgn_-tk$d8l)+e4np-kni+km8mfplzuxh^^@av
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
